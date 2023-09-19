83527b9504db2e6a22ece14a76403c6ca88fb32e
Felix Idalia <flxidalia@gmail.com>
2023-09-19 10:34:05 +0000

    Added file, _strLen_file.c

---
+++ _strLen_file.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_func - string length
+ * @s: a string
+ * Return: returns length length
+ */
+
+int _strlen_func(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (0);
+}
