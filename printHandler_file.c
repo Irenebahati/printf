76d599821a81b16d3b34d498229e921918765e5d
Felix Idalia <flxidalia@gmail.com>
2023-09-19 12:33:29 +0000

    File added, printHandler_file.c

---
+++ printHandler_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * handlePrint_func - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handlePrint_func(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', printChar_func}, {'s', printString_func}, {'%', printPercent_func},
+		{'i', printInt_func}, {'d', printInt_func}, {'b', printBinary_func},
+		{'u', printUnsigned_func}, {'o', printOctal_func},
+		{'X', printHexaUpper_func}, {'p', printPointer_func},
+		{'r', printReverse_func}, {'R', printRot13string_func},
+		{'x', printHexadecimal_func}, {'S', printNonPrintable_func},
+		{'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
